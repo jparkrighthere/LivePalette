36758ab74a949be24c8c71e38b61f6354c2d564a
= <raphy0316@naver.com>
2025-01-16 18:11:18 -0600

    ♻️ refactor : global redis EmailUtil 내부로 이동

--- src/main/java/com/example/demo/user/email/EmailUtil.java
+++ src/main/java/com/example/demo/user/email/EmailUtil.java
@@ -1,24 +1,42 @@
 package com.example.demo.user.email;
 
-import com.example.demo.global.util.RedisUtil;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
 
 @Service
+@RequiredArgsConstructor
 public class EmailUtil {
-    @Autowired
-    private RedisUtil redisUtil;
     private int authNumber;
     @Autowired
     private JavaMailSender javaMailSender;
 
+    private final StringRedisTemplate redisTemplate;
+
+    public String getData(String key){
+        ValueOperations<String,String> valueOperations=redisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    //지정된 키(key)에 값을 저장하고, 지정된 시간(duration) 후에 데이터가 만료되도록 설정하는 메서드
+    public void setDataExpire(String key,String value,long duration){
+        ValueOperations<String,String> valueOperations=redisTemplate.opsForValue();
+        Duration expireDuration= Duration.ofSeconds(duration);
+        valueOperations.set(key,value,expireDuration);
+    }
+
+
     public boolean CheckAuthNum(String email, String authNum){
-        return redisUtil.getData(authNum) != null && redisUtil.getData(authNum).equals(email);
+        return getData(authNum) != null && getData(authNum).equals(email);
     }
 
     public void makeRandomNumber() {
@@ -35,7 +53,7 @@ public class EmailUtil {
             helper.setTo(receiverEmail);
             helper.setSubject("회원 가입 인증 이메일 입니다.");
             helper.setText("",
-                    "나의 APP을 방문해주셔서 감사합니다." +
+                    "Live Palette를 방문해주셔서 감사합니다." +
                             "<br><br>" +
                             "인증 번호는 " + authNumber + "입니다." +
                             "<br>" +
@@ -55,7 +73,7 @@ public class EmailUtil {
             return null;
         }
         javaMailSender.send(message);
-        redisUtil.setDataExpire(Integer.toString(authNumber),receiverEmail,60*5L);
+        setDataExpire(Integer.toString(authNumber),receiverEmail,60*5L);
 
         return Integer.toString(authNumber);
     }
